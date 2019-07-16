875bab976b6bedebb55b11a24656453b77f7199b
jkallini <jkallini@princeton.edu>
2019-07-16 15:52:35 -0700

    Add "Students" and "Queue" tabs

--- app/src/main/java/com/example/helpq/AdminActivity.java
+++ app/src/main/java/com/example/helpq/AdminActivity.java
@@ -1,7 +1,10 @@
 package com.example.helpq;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -10,6 +13,21 @@ public class AdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.top_navigation);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_view_queue:
+                        // do something here
+                        return true;
+                    case R.id.action_view_students:
+                        // do something here
+                        return true;
+                    default: return true;
+                }
+            }
+        });
     }
 }
