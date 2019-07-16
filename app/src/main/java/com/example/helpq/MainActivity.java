1d2db95c62a78b80baf3a0af8b1ea1c2feff9cb6
jkallini <jkallini@princeton.edu>
2019-07-16 14:27:26 -0700

    Test pull request

--- app/src/main/java/com/example/helpq/MainActivity.java
+++ app/src/main/java/com/example/helpq/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helpq;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("MainActivity", "Testing out pull requests!");
     }
 }
