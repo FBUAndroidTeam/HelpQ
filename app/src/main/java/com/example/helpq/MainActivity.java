7cd5ab0ec209fb4d53f4e460c31c18cb41650d1f
jkallini <jkallini@princeton.edu>
2019-07-16 15:44:49 -0700

    Add top NavigationView, AdminActivity, and admin layout

--- app/src/main/java/com/example/helpq/MainActivity.java
+++ app/src/main/java/com/example/helpq/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.helpq;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("MainActivity", "Testing out pull requests!");
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.top_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_favorites:
+                        // do something here
+                        return true;
+                    default: return true;
+                }
+            }
+        });
     }
 }
