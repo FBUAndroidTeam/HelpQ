2341a4cb2575597b144c188aa0695c621f54bbbd
jkallini <jkallini@princeton.edu>
2019-07-17 16:37:05 -0700

    Wire Student vs. Admin login

--- app/src/main/java/com/example/helpq/view/LoginActivity.java
+++ app/src/main/java/com/example/helpq/view/LoginActivity.java
@@ -48,7 +48,7 @@ public class LoginActivity extends AppCompatActivity {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+                //startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
             }
         });
     }
