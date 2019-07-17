8433e6eb4f2339aecbca1030efbebb35f8df3cc2
jkallini <jkallini@princeton.edu>
2019-07-16 17:42:34 -0700

    Prepare for merge with master

---
+++ app/src/main/java/com/example/helpq/AdminQueueFragment.java
@@ -0,0 +1,20 @@
+package com.example.helpq;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AdminQueueFragment extends Fragment {
+
+    public static final String TAG = "AdminQueueFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_admin_queue, container, false);
+    }
+}
