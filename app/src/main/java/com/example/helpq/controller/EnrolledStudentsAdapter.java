2ce0b7ede8af47d7c41269586d3e46bf5f4ddfb1
jkallini <jkallini@princeton.edu>
2019-07-17 17:30:27 -0700

    Complete Swipe to Refresh

--- app/src/main/java/com/example/helpq/controller/EnrolledStudentsAdapter.java
+++
@@ -1,6 +0,0 @@
-package com.example.helpq.controller;
-
-import android.support.v7.widget.RecyclerView;
-
-public class EnrolledStudentsAdapter extends RecyclerView {
-}
--- app/src/main/java/com/example/helpq/view/QueueFragment.java
+++ app/src/main/java/com/example/helpq/view/QueueFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -27,6 +28,7 @@ public class QueueFragment extends Fragment {
     private RecyclerView rvQuestions;
     private List<Question> mQuestions;
     private QueueAdapter adapter;
+    private SwipeRefreshLayout swipeContainer;
 
     @Nullable
     @Override
@@ -46,15 +48,44 @@ public class QueueFragment extends Fragment {
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         rvQuestions.setLayoutManager(layoutManager);
 
-        queryQuestions(getString(R.string.PRIORITY_BLOCKING));
-        queryQuestions(getString(R.string.PRIORITY_STRETCH));
-        queryQuestions(getString(R.string.PRIORITY_CURIOSITY));
+        queryQuestions();
+        setupSwipeRefreshing(view);
+    }
 
+    // Handle logic for Swipe to Refresh.
+    private void setupSwipeRefreshing(@NonNull View view) {
+
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchQueueAsync();
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
 
     }
 
+    // Refresh the queue, and load questions.
+    protected void fetchQueueAsync() {
+        adapter.clear();
+        queryQuestions();
+        swipeContainer.setRefreshing(false);
+    }
 
-    private void queryQuestions (final String priorityEmoji) {
+    private void queryQuestions() {
+        queryQuestionsWithPriority(getString(R.string.PRIORITY_BLOCKING));
+        queryQuestionsWithPriority(getString(R.string.PRIORITY_STRETCH));
+        queryQuestionsWithPriority(getString(R.string.PRIORITY_CURIOSITY));
+    }
+
+    private void queryQuestionsWithPriority(final String priorityEmoji) {
         final ParseQuery<Question> questionQuery = new ParseQuery<Question>(Question.class);
         questionQuery.findInBackground(new FindCallback<Question>() {
             @Override
