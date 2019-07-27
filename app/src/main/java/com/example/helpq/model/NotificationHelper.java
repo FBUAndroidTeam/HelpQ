63ef5596a9298f916a91c122c1453344180b5946
jkallini <jkallini@princeton.edu>
2019-07-26 19:45:31 -0700

    NotificationHelper can remove badges from items in NavigationView

--- app/src/main/java/com/example/helpq/model/NotificationHelper.java
+++ app/src/main/java/com/example/helpq/model/NotificationHelper.java
@@ -9,44 +9,22 @@ import android.widget.TextView;
 
 import com.example.helpq.R;
 
-public class NotificationAdder {
+public class NotificationHelper {
 
     private BottomNavigationView mNavigationView;
     private Context mContext;
     private final int MAX_NOTIFICATIONS = 100;
 
-    public NotificationAdder(BottomNavigationView navigationView, Context context) {
+    // Constructor
+    public NotificationHelper(BottomNavigationView navigationView, Context context) {
         mNavigationView = navigationView;
         mContext = context;
     }
 
-    public void setProfileTabNotification(int number) {
-        setNotificationBadge(R.id.action_profile, number);
-    }
-
-    public void setEnrolledTabNotification(int number) {
-        setNotificationBadge(R.id.action_enrolled, number);
-    }
-
-    public void setQueueTabNotification(int number) {
-        setNotificationBadge(R.id.action_queue, number);
-    }
-
-    public void setWorkshopTabNotification(int number) {
-        setNotificationBadge(R.id.action_workshop, number);
-    }
-
-    public void setBoardTabNotification(int number) {
-        setNotificationBadge(R.id.action_board, number);
-    }
-
-    public void setInboxTabNotification(int number) {
-        setNotificationBadge(R.id.action_inbox, number);
-    }
-
-    private void setNotificationBadge(int layoutId, int number)
+    // Add a notification badge to the item with given id.
+    public void addBadge(int itemId, int number)
     {
-        BottomNavigationItemView notificationsTab = mNavigationView.findViewById(layoutId);
+        BottomNavigationItemView notificationsTab = mNavigationView.findViewById(itemId);
         View badge = LayoutInflater.from(mContext)
                 .inflate(R.layout.notification_badge, notificationsTab, false);
         TextView notificationBadgeTextView = badge.findViewById(R.id.tvNotificationBadge);
@@ -57,5 +35,13 @@ public class NotificationAdder {
                     mContext.getResources().getString(R.string.max_notifications));
         }
         notificationsTab.addView(badge);
+    }
+
+    // Remove the notification badge from the item with the given id, if it has a badge.
+    public void removeBadge(int itemId) {
+        BottomNavigationItemView notificationsTab = mNavigationView.findViewById(itemId);
+        if (notificationsTab.getChildCount() > 2) {
+            notificationsTab.removeViewAt(2);
+        }
     }
 }
