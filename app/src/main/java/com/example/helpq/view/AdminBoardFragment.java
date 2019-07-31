c5901342c84ae9a19df38fec725766c3a8244673
jkallini <jkallini@princeton.edu>
2019-07-31 14:40:56 -0700

    Implement search in AdminBoardFragment

--- app/src/main/java/com/example/helpq/view/AdminBoardFragment.java
+++ app/src/main/java/com/example/helpq/view/AdminBoardFragment.java
@@ -2,9 +2,11 @@ package com.example.helpq.view;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.SearchView;
 
 import com.example.helpq.model.QueryFactory;
 import com.example.helpq.model.Question;
+import com.example.helpq.model.Search;
 import com.example.helpq.model.User;
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -47,6 +49,35 @@ public class AdminBoardFragment extends InboxFragment {
                 if (mMessages.size() == 0) {
                     tvNotice.setVisibility(View.VISIBLE);
                 }
+            }
+        });
+    }
+
+    protected void search() {
+        svSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(final String query) {
+                final ParseQuery<Question> boardQuestions =
+                        QueryFactory.QuestionQuery.getAdminBoardMessages();
+                boardQuestions.findInBackground(new FindCallback<Question>() {
+                    @Override
+                    public void done(List<Question> objects, ParseException e) {
+                        List<Question> result = Search.mSearch(objects, query);
+                        mMessages.clear();
+                        mMessages.addAll(result);
+                        mAdapter.notifyDataSetChanged();
+                    }
+                });
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText.isEmpty()) {
+                    mMessages.clear();
+                    queryMessages();
+                }
+                return false;
             }
         });
     }
