cbc989052b78fc7b34097d58d93d77a9cf8bcf5f
jkallini <jkallini@princeton.edu>
2019-08-08 11:49:53 -0700

    Add stats to admin profile page

--- app/src/main/java/com/example/helpq/view/admin_views/AdminProfileFragment.java
+++ app/src/main/java/com/example/helpq/view/admin_views/AdminProfileFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,11 +15,18 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.helpq.R;
+import com.example.helpq.model.QueryFactory;
 import com.example.helpq.model.User;
+import com.example.helpq.model.WaitTime;
 import com.example.helpq.view.LoginActivity;
 import com.example.helpq.view.MainActivity;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class AdminProfileFragment extends Fragment {
 
@@ -27,6 +35,7 @@ public class AdminProfileFragment extends Fragment {
     private SimpleDraweeView ppvPicture;
     private TextView tvFullName;
     private TextView tvUsername;
+    private TextView tvAnsweredQuestions;
     private Button btnLogout;
 
     public static AdminProfileFragment newInstance() {
@@ -48,11 +57,42 @@ public class AdminProfileFragment extends Fragment {
         tvFullName = view.findViewById(R.id.tvFullName);
         btnLogout = view.findViewById(R.id.btnLogout);
         ppvPicture = view.findViewById(R.id.ppvPicture);
+        tvAnsweredQuestions = view.findViewById(R.id.tvAnsweredQuestions);
         tvFullName.setText(User.getFullName(ParseUser.getCurrentUser()));
         tvUsername.setText(ParseUser.getCurrentUser().getUsername());
         setupLogout();
         ppvPicture.setImageURI(Uri.parse("http://graph.facebook.com/"+
                 User.getProfilePicture(ParseUser.getCurrentUser())+"/picture?type=large"));
+
+        setupStatsText();
+    }
+
+    private void setupStatsText() {
+        ParseQuery<WaitTime> query = QueryFactory.WaitTimes.getAdminWaitTimes();
+        query.findInBackground(new FindCallback<WaitTime>() {
+            @Override
+            public void done(List<WaitTime> objects, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Error querying for admin wait time!");
+                    return;
+                }
+                if (objects.size() > 1) {
+                    Log.e(TAG, "Every admin should only have one WaitTime dataset!");
+                    return;
+                }
+                WaitTime waitTime;
+                if (objects.size() == 0) {
+                    waitTime = new WaitTime();
+                    waitTime.setAdminName(User.getAdminName(ParseUser.getCurrentUser()));
+                    waitTime.saveInBackground();
+                } else {
+                    waitTime = objects.get(0);
+                }
+                long totalAnswered = waitTime.getBlockingSize() + waitTime.getStretchSize() +
+                        waitTime.getCuriositySize();
+                tvAnsweredQuestions.setText(String.format("%d answered questions", totalAnswered));
+            }
+        });
     }
 
     @Override
